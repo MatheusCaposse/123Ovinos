aab6df67839a62a2a190b6161812cf7790ba47ec
Matheus C <matheus.caposse02@gmail.com>
2026-02-23 21:22:46 -0300

    criando projeto

---
+++ src/test/java/com/ovinos/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ovinos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
