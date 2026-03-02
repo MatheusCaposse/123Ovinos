d41f2726bb85825f764017de7b6807eaf83c61b7
Matheus C <matheus.caposse02@gmail.com>
2026-03-02 18:58:31 -0300

    criando endpoint GetStatusBySex()

--- src/main/java/com/ovinos/resource/SheepResource.java
+++ src/main/java/com/ovinos/resource/SheepResource.java
@@ -1,5 +1,8 @@
 package com.ovinos.resource;
 
+import com.ovinos.entity.Enum.ConditionSheep;
+import com.ovinos.entity.Enum.SheepSex;
+import com.ovinos.entity.Enum.SheepStatus;
 import com.ovinos.entity.superClass.Sheep;
 import com.ovinos.service.SheepService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +12,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
 import java.util.List;
 
 @RestController
@@ -28,5 +32,12 @@ public class SheepResource {
     public ResponseEntity<Sheep> findById(@PathVariable String id){
         Sheep obj = service.findById(id);
         return ResponseEntity.ok().body(obj);
+    }
+
+    @GetMapping("/status/{sex}")
+    public List<ConditionSheep> getStatusBySex(@PathVariable SheepSex sex) {
+        return Arrays.stream(ConditionSheep.values())
+                .filter(status -> status.canBe(sex))
+                .toList();
     }
 }
