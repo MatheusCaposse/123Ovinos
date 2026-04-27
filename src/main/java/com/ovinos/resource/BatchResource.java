8a05d43aa6838fd1e099ad2dfeced8b9d8f0d42e
Matheus C <matheus.caposse02@gmail.com>
2026-04-27 13:50:52 -0300

    adicionando permisao para comunicacao do back-end com front-end

--- src/main/java/com/ovinos/resource/BatchResource.java
+++ src/main/java/com/ovinos/resource/BatchResource.java
@@ -9,9 +9,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping(value = "/batch")
 public class BatchResource {
