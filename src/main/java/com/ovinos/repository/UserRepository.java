3db140082b302791f40cd81f81fb5142609bfeb0
Matheus C <matheus.caposse02@gmail.com>
2026-05-15 18:18:43 -0300

    adiciondando PasswordEncoder

---
+++ src/main/java/com/ovinos/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.ovinos.repository;
+
+import com.ovinos.entity.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmailIgnoreCase(String email);
+}
---
+++ src/main/java/com/ovinos/service/UserService.java
@@ -0,0 +1,23 @@
+package com.ovinos.service;
+
+import com.ovinos.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService implements UserDetailsService {
+
+
+    @Autowired
+    private UserRepository repository;
+
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return repository.findByEmailIgnoreCase(username)
+                .orElseThrow(()-> new UsernameNotFoundException("Usuario não encontrado"));
+    }
+}
